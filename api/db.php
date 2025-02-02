ed8a8f4849bd9582685c5f103ad7a045a11ae510
jamilabarasa <jamilabarasa18@gmail.com>
2025-02-02 12:50:04 +0300

    added new database

--- api/db.php
+++ api/db.php
@@ -1,11 +1,11 @@
 <?php
-$host = 'sql209.infinityfree.com';
-$dbname = ' if0_38188415_customizable_receipts';
-$username = 'if0_38188415';
-$password = ' l7Rvm3OzGmYzt2'; 
+$host = '165.22.66.209';
+$dbname = 'customizable';
+$username = 'root';
+$password = 'customizable2025'; 
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=25306;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
